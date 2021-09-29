bed7c1323d0f7be9043465d8eeae9d06eefd21c0
Nikita Sokolov <mr.sokolov99@gmail.com>
2021-09-29 17:48:36 +0300

    test

--- src/08-shared/octokit/index.ts
+++ src/08-shared/octokit/index.ts
@@ -2,5 +2,5 @@
 import { Octokit } from "@octokit/core";
 
 export const octokit = new Octokit({
-  auth: `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`,
+  auth: `ghp_OGL  V6Uw8L6D2oDZQcLZoIw7BybbXzu3DP6Np`,
 });
